9d54f90926e3cea5c7769af2daca6f12b4d1af98
Aruzhan <190103285@stu.sdu.edu.kz>
2021-02-23 16:36:40 +0600

    commit message

--- lab4/routes/web.php
+++ lab4/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('name/{id?}', function () {
     return view('myName');
 });
+
+Route::get('name2/{id?}', function () {
+    return view('myName');
+});
+
+
+
+
